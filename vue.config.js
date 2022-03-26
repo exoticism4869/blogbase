0e7ce628a615976aa2ac64a5f614b1ce90328397
exoticism <1484296386@qq.com>
2022-03-26 20:44:22 +0800

    change to relative path

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
 	transpileDependencies: true,
 	productionSourceMap: false,
-	publicPath: '/',
+	publicPath: './',
 	outputDir: 'docs',
 	configureWebpack: (config) => {
 		//调试JS
