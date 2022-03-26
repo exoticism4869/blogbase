091e189c90bf2baf7d91afebc0a5d40d6d93e117
exoticism <1484296386@qq.com>
2022-03-26 20:24:46 +0800

    docs

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
 	transpileDependencies: true,
+	productionSourceMap: false,
+	publicPath: '/',
+	outputDir: 'docs',
 	configureWebpack: (config) => {
 		//调试JS
 		config.devtool = 'source-map'
